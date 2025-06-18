0617c3cf335b811a19af7abef6843c2dfb902eb9
dada2549 <ssonsongkram@gmail.com>
2025-06-18 13:27:05 +0000

    add

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,10 @@
+def main():
+  params = sys.argv[1:]  
+  if len(params) == 0:
+     print("none")
+     return
+  for p in params:    
+     if p.endswith("ism"):
+        continue
+       print(p + "ism")
+ main()         
