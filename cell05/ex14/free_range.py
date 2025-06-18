d197ba0deb25e6d7f50c16ac34a8d8596bccdba4
dada2549 <ssonsongkram@gmail.com>
2025-06-18 13:37:57 +0000

    add

---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,13 @@
+def main():
+  if len(sys.argv) != 3:  
+     print("none")
+     return
+  try:  
+    start = int(sys.argv[1]) 
+    end = int(sys.argv[2])
+  except ValueError:
+    print("none")   
+    return
+  if start <= end:
+     arr = list(range(start, end + 1)
+  else:       
