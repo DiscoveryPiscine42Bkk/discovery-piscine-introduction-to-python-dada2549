f069464f6aa08747f4535fe2cf3fb4176a0fafa5
dada2549 <ssonsongkram@gmail.com>
2025-06-19 04:26:55 +0000

    add

--- cell07/ your_namebook.py
+++
@@ -1,6 +0,0 @@
-def array_of_names(name_dict):
-  full_names = [] 
-  for first, last in name_dict.items():
-   full_name = f"{first.capitalize()} {last.capitalize()}"
-   full_names.append(full_name)
-    return full_names  
